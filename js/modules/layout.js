22eb0424cc73168668a82de1dd6a5edc0f9906a1
Sergio Muriel <sergio.muriel@quiksilver.eu>
2015-04-18 21:16:04 +0200

    Click on title close tabs

--- js/modules/layout.js
+++ js/modules/layout.js
@@ -1,6 +1,7 @@
 var Layout = function()
 {
     var mover = document.querySelector('.slides_move');
+    var buttons=  document.querySelectorAll('.header button');
 
     this.init = function(controller)
     {
@@ -8,40 +9,40 @@ var Layout = function()
         this.bind();
     };
 
+    this.display_left= function()
+    {
+        mover.className='slides_move left_visible';
+        Array.forEach( buttons, function(button)
+        {
+            button.classList.remove('selected');
+        });
+        document.querySelector('.button_left').classList.add('selected');
+    };
+
     this.display_right= function()
     {
         mover.className='slides_move right_visible';
+        Array.forEach( buttons, function(button)
+        {
+            button.classList.remove('selected');
+        });
+        document.querySelector('.button_right').classList.add('selected');
     };
+
     this.display_center= function()
     {
         mover.className='slides_move center_visible';
+        Array.forEach( buttons, function(button)
+        {
+            button.classList.remove('selected');
+        });
     };
 
     this.bind =  function()
     {
-        var buttons=  document.querySelectorAll('.header button');
-
-        Array.forEach( buttons, function(button)
-        {
-            button.addEventListener('click', function()
-            {
-                var slide_class = this.getAttribute('data-slide')+'_visible';
-                Array.forEach( buttons, function(button)
-                {
-                    button.classList.remove('selected');
-                });
-
-                if(!mover.classList.contains(slide_class))
-                {
-                    this.classList.add('selected');
-                    mover.className='slides_move '+slide_class;
-                }
-                else
-                {
-                    mover.className='slides_move center_visible';
-                }
-            });
-        });
+        document.querySelector('.header h1').addEventListener('click', this.display_center);
+        document.querySelector('.header .button_left').addEventListener('click', this.display_left);
+        document.querySelector('.header .button_right').addEventListener('click', this.display_right);
     };
 };
 

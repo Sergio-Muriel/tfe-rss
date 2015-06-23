a4d2355477157409693df85fb425dcd06ff2d2ad
sergio muriel <sergio.muriel@quiksilver.eu>
2015-06-23 10:06:23 +0200

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 grunt build
-zip -r output.zip callback_feedly.html  css/ data/ img/ index.html  js/ LICENSE  manifest.webapp   README.md  src/ TODO.md 
+cd build
+zip -r output.zip *
+cd ..
